ccd0dfe666ef439d8a55337abf22d370fbcbd55a
Daelo Namaumbo <spikkernamaumbo>
2021-07-19 16:42:46 +0200

    schema validationn

--- middlewares/signUpValidator.js
+++ middlewares/signUpValidator.js
@@ -37,6 +37,6 @@ exports.singUpValidator = [
     const errors = validationResult(req);
     if (!errors.isEmpty())
       return res.status(422).json({ errors: errors.array() });
-    // next();
+    next();
   },
 ];
