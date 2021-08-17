417e23d80149f1ed75d29b196af3c1b011aeb890
Daelo Namaumbo <spikkernamaumbo>
2021-08-17 14:06:22 +0200

    deploying

--- DatabaseConnection.js
+++ DatabaseConnection.js
@@ -1,16 +1,14 @@
 const {Sequelize} = require("sequelize")
 require('dotenv').config()
 
-const connection = new Sequelize("chancoNotify","postgres","spikker6!",{
-    host: 'localhost',
-    dialect: 'postgres',
-    pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
+const connection = new Sequelize(process.env.DATABASE_URL,{
+    dialectOptions:{
+      ssl:{
+        required: true, 
+        rejectUnauthorized:false,
+      }
     }
   });
-// connection.sync({force: true})
+ connection.sync({force: true})
 
   module.exports = connection
