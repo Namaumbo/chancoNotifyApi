c2d57502691bfa6c450e8c5691ff9078f00c9e00
Daelo Namaumbo <spikkernamaumbo>
2021-08-19 21:12:12 +0200

    delete

--- DatabaseConnection.js
+++ DatabaseConnection.js
@@ -26,6 +26,6 @@ const connection = new Sequelize(process.env.DATABASE_URL,{
 //     },
 //   }
 // );
-connection.sync({force: true})
+// connection.sync({force: true})
 
 module.exports = connection;
