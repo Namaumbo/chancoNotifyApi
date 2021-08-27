6837aec38b92f871d4d4d7208dad25009dba0d6e
Daelo Namaumbo <spikkernamaumbo>
2021-08-27 11:13:12 +0200

    committing

--- DatabaseConnection.js
+++ DatabaseConnection.js
@@ -1,30 +1,30 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-// const connection = new Sequelize(process.env.DATABASE_URL,{
-//     dialectOptions:{
-//       ssl:{
-//         required: true,
-//         rejectUnauthorized:false,
-//       }
-//     }
-//   });
+const connection = new Sequelize(process.env.DATABASE_URL,{
+    dialectOptions:{
+      ssl:{
+        required: true,
+        rejectUnauthorized:false,
+      }
+    }
+  });
 
 
-const connection = new Sequelize(
-  process.env.DB_NAME,
-  "postgres",
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "postgres",
-    pool: {
-      idle: 10000,
-      max: 30,
-      min: 1,
-      acquire: 30000,
-    },
-  }
+// const connection = new Sequelize(
+//   process.env.DB_NAME,
+//   "postgres",
+//   process.env.DB_PASSWORD,
+//   {
+//     host: "localhost",
+//     dialect: "postgres",
+//     pool: {
+//       idle: 10000,
+//       max: 30,
+//       min: 1,
+//       acquire: 30000,
+//     },
+//   }
 
 );
 // connection.sync({force: true})
